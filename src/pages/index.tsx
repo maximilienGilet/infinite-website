62e4ffb1a64a8acb272f242ffdaae881b007e697
Maximilien Gilet <maximilien248@gmail.com>
2023-06-10 19:07:07 +0200

    add example links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 
+const examples: string[] = [
+  "/recipes/cookies/chocolate-chip",
+  "/blog/2021/08/01/first-post",
+  "/forum/programming/javascript/index.html",
+  "/how-to/fix-a-broken-toaster",
+];
+
 export default function Home() {
   const inputRef = React.useRef<HTMLInputElement>(null);
 
@@ -13,6 +20,7 @@ export default function Home() {
       }
     }
   };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="text-center">
@@ -20,10 +28,10 @@ export default function Home() {
         <br />
         <p>Explore any link and discover anything. This website is endless.</p>
       </div>
-      <div className="w-screen sm:w-96">
+      <div className="w-screen sm:w-96 mx-4">
         <form
           onSubmit={handleForm}
-          className="border-4 border-black mx-4 flex transition-all focus-within:-translate-x-1 focus-within:-translate-y-1 focus-within:shadow-[5px_5px_black]"
+          className="border-4 border-black flex transition-all focus-within:-translate-x-1 focus-within:-translate-y-1 focus-within:shadow-[5px_5px_black]"
         >
           <p className="py-4 pl-4 pr-2">/</p>
           <input
@@ -31,7 +39,7 @@ export default function Home() {
             className="w-full bg-transparent outline-none"
             type="search"
             placeholder="show-me-anything"
-            autoFocus
+            autoFocus={true}
             pattern={`[a-z][^\\\\\:\|\<\>\"\*\?]*$`}
           />
           <button
@@ -41,6 +49,20 @@ export default function Home() {
             GO
           </button>
         </form>
+        <div className="mt-4">
+          <ul className="mt-2">
+            {examples.map((example) => (
+              <li key={example}>
+                <a
+                  className="p-2 px-4 border-2 border-black mb-2 w-full block hover:bg-black hover:text-white transition-all"
+                  href={`${example}`}
+                >
+                  {example}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
       <div></div>
     </main>
